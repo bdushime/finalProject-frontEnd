b6351cf93a050940df7082f3af75fe2f2f05028b
Prom004 <izerepromise004@gmail.com>
2026-03-16 14:55:44 +0200

    fix: updating the backend link

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: 'http://localhost:5001/api',
+    baseURL: 'https://equipment-tracker-backend-dfso.onrender.com/api',
 
     headers: {
         'Content-Type': 'application/json',
